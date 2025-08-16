b109f0060bdb79eccea696eeff9a7522d706b5a9
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-16 16:07:20 -0300

    feat: Testing fonts and router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { View, Text, Button } from "react-native";
 import { router } from "expo-router";
+import { fontFamily } from "@/theme/fontFamily";
 
 export default function App() {
   return (
@@ -10,8 +11,16 @@ export default function App() {
         justifyContent: "center",
       }}
     >
-      <Text>Hello</Text>
+      <Text style={{ fontFamily: fontFamily.bold }}>Hello</Text>
       <Button title="Click me" onPress={() => router.navigate("/target")} />
+      <Button
+        title="Go to Transaction"
+        onPress={() => router.navigate("/transaction/123")}
+      />
+      <Button
+        title="Go to In Progress"
+        onPress={() => router.navigate("/in-progress/123")}
+      />
     </View>
   );
 }
--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -1,12 +1,13 @@
-import { View, Text } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import { View, Text, Button } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
 
 export default function Transaction() {
-  const { id } = useLocalSearchParams();
+  const { id } = useLocalSearchParams<{ id: string }>();
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Id: {id}</Text>
+      <Button title="Go to Home" onPress={() => router.back()} />
     </View>
   );
 }
