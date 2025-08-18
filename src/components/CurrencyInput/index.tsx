679eeb28bddc52ea6cf2193df60db40f1675ed65
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-17 23:09:06 -0300

    feat: add form input components (CurrencyInput, Input)

---
+++ src/components/CurrencyInput/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+import { colors } from "@/theme";
+import Input, { CurrencyInputProps } from "react-native-currency-input";
+
+type Props = CurrencyInputProps & {
+  label: string;
+};
+
+export function CurrencyInput({ label, ...rest }: Props) {
+  return (
+    <View>
+      <Text style={styles.label}>{label}</Text>
+      <Input
+        style={styles.input}
+        prefix="$ "
+        delimiter="."
+        separator=","
+        precision={2}
+        minValue={0}
+        placeholderTextColor={colors.gray[400]}
+        {...rest}
+      />
+    </View>
+  );
+}
