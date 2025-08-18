921e6941ad6f11fdd5e8a6fcd3e7b9bf6951ad11
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-17 23:13:05 -0300

    feat: update in-progress page with new components

--- src/app/in-progress/[id].tsx
+++ src/app/in-progress/[id].tsx
@@ -1,13 +1,62 @@
 import { router, useLocalSearchParams } from "expo-router";
-import { View, Text, Button } from "react-native";
+import { View, Text } from "react-native";
+import { PageHeader } from "@/components/PageHeader";
+import { Progress } from "@/components/Progress";
+import { Transaction } from "@/components/Transaction";
+import { List } from "@/components/List";
+import { TransactionTypes } from "@/utils/TransactionTypes";
+import { Button } from "@/components/Button";
 
 export default function InProgress() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
+  const details = {
+    current: "1000",
+    target: "1000",
+    percentage: 1000,
+  };
+
+  const transactions = [
+    {
+      id: "1",
+      value: "100",
+      date: "2021-01-01",
+      description: "Apple Watch",
+      type: TransactionTypes.Input,
+    },
+    {
+      id: "2",
+      value: "100",
+      date: "2021-01-01",
+      description: "Apple Watch",
+      type: TransactionTypes.Output,
+    },
+  ];
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Id: {id}</Text>
-      <Button title="Go to Home" onPress={() => router.back()} />
+    <View style={{ flex: 1, padding: 24, gap: 32 }}>
+      <PageHeader
+        title="Apple Watch"
+        rightButton={{
+          icon: "edit",
+          onPress: () => router.navigate("/target"),
+        }}
+      />
+      <Progress data={details} />
+      <List
+        title="Transactions"
+        data={transactions}
+        renderItem={({ item }) => (
+          <Transaction data={item} onRemove={() => {}} />
+        )}
+        emptyMessage="No transactions found"
+      />
+      <Button
+        title="Add Transaction"
+        onPress={() => {
+          router.navigate(`/transaction/${id}`);
+        }}
+      />
     </View>
   );
 }
