a8940ca594317ab2e465c298fe9aaa39b507cbc3
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-17 23:06:38 -0300

    feat: Implementing Components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,71 @@
-import { View, Text, Button } from "react-native";
+import { View, StatusBar } from "react-native";
 import { router } from "expo-router";
-import { fontFamily } from "@/theme/fontFamily";
+import { HomeHeader } from "@/components/HomeHeader";
+import { Target, TargetProps } from "@/components/Target";
+import { List } from "@/components/List";
+import { Button } from "@/components/Button";
+
+const summaryData = {
+  total: "$100,00",
+  input: { label: "In", value: "$100,00" },
+  output: { label: "Out", value: "- $100,00" },
+};
+
+const targetsData: TargetProps[] = [
+  {
+    id: "1",
+    name: "Car",
+    percentage: "100%",
+    current: "$100,00",
+    target: "$100,00",
+  },
+  {
+    id: "2",
+    name: "House",
+    percentage: "50%",
+    current: "$50,00",
+    target: "$100,00",
+  },
+  {
+    id: "3",
+    name: "Vacation",
+    percentage: "25%",
+    current: "$25,00",
+    target: "$100,00",
+  },
+  {
+    id: "4",
+    name: "Car",
+    percentage: "100%",
+    current: "$100,00",
+    target: "$100,00",
+  },
+];
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text style={{ fontFamily: fontFamily.bold }}>Hello</Text>
-      <Button title="Click me" onPress={() => router.navigate("/target")} />
-      <Button
-        title="Go to Transaction"
-        onPress={() => router.navigate("/transaction/123")}
+    <View style={{ flex: 1 }}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
       />
-      <Button
-        title="Go to In Progress"
-        onPress={() => router.navigate("/in-progress/123")}
+      <HomeHeader data={summaryData} />
+      <List
+        title="Targets"
+        emptyMessage="No targets found"
+        data={targetsData}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
+        containerStyle={{ paddingHorizontal: 24 }}
       />
+      <View style={{ padding: 24, paddingBottom: 32 }}>
+        <Button title="Add Target" onPress={() => router.navigate("/target")} />
+      </View>
     </View>
   );
 }
