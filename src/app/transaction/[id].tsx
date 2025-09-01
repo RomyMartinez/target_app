17aa30d5740e9d9d1be74967bd994698f70a5800
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-08-31 22:00:27 -0300

    feat: Implementing Option Component and Enhancing Transaction Page

--- src/app/transaction/[id].tsx
+++ src/app/transaction/[id].tsx
@@ -5,15 +5,25 @@ import { PageHeader } from "@/components/PageHeader";
 import { CurrencyInput } from "@/components/CurrencyInput";
 import { Input } from "@/components/Input";
 import { Button } from "@/components/Button";
+import { TransactionType } from "@/components/TransactionType";
+import { useState } from "react";
+import { TransactionTypes } from "@/utils/TransactionTypes";
 
 export default function Transaction() {
   const { id } = useLocalSearchParams<{ id: string }>();
+  const [transactionType, setTransactionType] = useState(
+    TransactionTypes.Input,
+  );
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader title="Transaction Details" />
 
       <View style={{ marginTop: 32, gap: 24 }}>
+        <TransactionType
+          selected={transactionType}
+          onChange={setTransactionType}
+        />
         <CurrencyInput label="Value ($)" value={0} />
         <Input label="Motive (Optional)" placeholder="Ex: Groceries, Rent..." />
         <Button title="Save" onPress={() => {}} />
